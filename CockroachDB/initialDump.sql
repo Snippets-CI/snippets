4ee26de7754cd1ae35166b1c97692b952998fda9
MarkusSeiberl <markus_seiberl@live.at>
2020-05-17 14:06:44 +0200

    updates sql dump

--- CockroachDB/initialDump.sql
+++ CockroachDB/initialDump.sql
@@ -1,24 +1,27 @@
 CREATE TABLE users (
-        userid INT8 NOT NULL,
+        user_id UUID NOT NULL DEFAULT gen_random_uuid(),
         username VARCHAR(25) NOT NULL,
         password VARCHAR(60) NOT NULL,
-        CONSTRAINT "primary" PRIMARY KEY (userid ASC),
-        FAMILY "primary" (userid, username, password)
+        CONSTRAINT "primary" PRIMARY KEY (user_id ASC),
+        FAMILY "primary" (user_id, username, password)
 );
 
 CREATE TABLE snippets (
-        userid INT8 NOT NULL,
-        owner INT8 NOT NULL,
+        snippet_id UUID NOT NULL DEFAULT gen_random_uuid(),
+        owner UUID NOT NULL,
         language VARCHAR(20) NULL,
         title VARCHAR(30) NULL,
         category VARCHAR(50) NULL,
         code VARCHAR NULL,
-        CONSTRAINT "primary" PRIMARY KEY (userid ASC),
+        CONSTRAINT "primary" PRIMARY KEY (snippet_id ASC),
         INDEX snippets_auto_index_fk_owner_ref_users (owner ASC),
-        FAMILY "primary" (userid, owner, language, title, category, code)
+        FAMILY "primary" (snippet_id, owner, language, title, category, code)
 );
 
-ALTER TABLE snippets ADD CONSTRAINT fk_owner_ref_users FOREIGN KEY (owner) REFERENCES users(userid);
+INSERT INTO users (user_id, username, password) VALUES
+        ('99ce9c3c-6210-4ed0-a759-5d289f4d2cd0', 'markus', 'clearPassword');
+
+ALTER TABLE snippets ADD CONSTRAINT fk_owner_ref_users FOREIGN KEY (owner) REFERENCES users(user_id);
 
 -- Validate foreign key constraints. These can fail if there was unvalidated data during the dump.
 ALTER TABLE snippets VALIDATE CONSTRAINT fk_owner_ref_users;
