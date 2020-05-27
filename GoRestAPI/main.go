253766823eb7474d7269c20c410a5ba1783883ab
Andreas Roither <andi.roither@protonmail.com>
2020-05-27 12:32:40 +0200

    Add swagger

--- GoRestAPI/main.go
+++ GoRestAPI/main.go
@@ -1,3 +1,21 @@
+// Snippets
+//
+// The purpose of this application is to serve Snippets
+//
+//     Schemes: http
+//     Host: localhost:8010
+//     Version: 0.0.1
+//     License: MIT http://opensource.org/licenses/MIT
+//     Contact: Andreas | Markus <andi.roither@protonmail.com>
+//
+//     Consumes:
+//     - text/plain
+//
+//     Produces:
+//     - text/plain
+//
+// swagger:meta
+//go:generate swagger generate spec
 package main
 
 import (
