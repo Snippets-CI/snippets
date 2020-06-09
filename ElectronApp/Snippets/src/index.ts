1a348cfd238a85bef8bd5d4b038d5d223b66403f
Andreas Roither <andi.roither@protonmail.com>
2020-06-09 11:08:26 +0200

    Fix sonarcloud issues for electron

--- ElectronApp/Snippets/src/index.ts
+++ ElectronApp/Snippets/src/index.ts
@@ -1,9 +1,6 @@
 import { app, BrowserWindow, ipcMain } from "electron";
-import path from "path";
-import url from "url";
 
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
-declare const SPLASH_WINDOW_WEBPACK_ENTRY: any;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
