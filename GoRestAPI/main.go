3b9601b2115a727d888e8126e57070b43105b40f
Andreas Roither <andi.roither@protonmail.com>
2020-06-18 13:26:07 +0200

    Update heroku rest api port

--- GoRestAPI/main.go
+++ GoRestAPI/main.go
@@ -46,5 +46,5 @@ func main() {
 	// should be done by os.Getenv("")
 	a.Initialize(os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"), os.Getenv("POSTGRES_HOST_NAME"), true)
 
-	a.Run(":" + os.Getenv("REST_API_PORT"))
+	a.Run(":" + os.Getenv("PORT"))
 }
