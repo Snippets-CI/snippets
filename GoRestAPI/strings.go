0d2052e4afb8f73f2b3d19bcd1ac992b0035290c
MarkusSeiberl <markus_seiberl@live.at>
2021-01-20 21:39:50 +0100

    New Welcome message

--- GoRestAPI/strings.go
+++ GoRestAPI/strings.go
@@ -1,5 +1,5 @@
 package main
 
-const WelcomeMessage = "Welcome to SNIPPETS!"
+const WelcomeMessage = "Welcome to SNIPPETS on Azure!"
 const ErrorMessage = "Error Resource not found"
 const ErrorMessageMalformedJSON = "Invalid Json found"
